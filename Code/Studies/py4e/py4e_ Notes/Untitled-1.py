c03f1a284a03f6ea2d578d3b4ba7716171996873
inquiz <62821667+Inquiz@users.noreply.github.com>
2023-09-09 17:04:36 -0500

    Cleaning out files

--- Code/Studies/py4e/py4e_ Notes/Untitled-1.py
+++
@@ -1,7 +0,0 @@
-hrs = input("Enter Hours:")
-    hrsf=float(hrs)
-rate = input("Enter Pay Rate")
-    ratef=float(rate)
-pay=(hrsf*ratef)
-print("Pay:", pay)   
-  
--- Code/Studies/py4e/py4e_ Notes/Untitled-2.py
+++
@@ -1,7 +0,0 @@
-x=5
-if x<10:
-    print("Smaller")
-if x>20:
-    print("Bigger")
-    
-print('Finis')
