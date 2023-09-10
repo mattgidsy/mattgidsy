5c84883040c2862199d9126474230628fac52494
inquiz go <62821667+Inquiz@users.noreply.github.com>
2023-09-10 02:37:03 -0500

    staying  up too late with silly soup

--- Code/Studies/py4e/py4e_ Notes/git_test_party_hard.py
+++ Code/Studies/py4e/py4e_ Notes/git_test_party_hard.py
@@ -2,10 +2,28 @@
 # Ask if a they are a party animal
 # Scrape data from a lyrics site that will print party for every instance of party in "Party Hard"
 # Rick roll the non-party animals
-import bs4
+import urllib.request, urllib.parse, urllib.error
+from  bs4 import BeautifulSoup
 import ssl
-import urllib
 
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+#parse the website for lyrics
+def soup_to_text():
+    url = "https://www.azlyrics.com/lyrics/andrewwk/partyhard.html"
+    html = urllib.request.urlopen(url, context=ctx).read()
+    soup = BeautifulSoup(html, 'html.parser')
+    text = soup.get_text()
+    words = text.split()
+    song = words[44:319]
+
+### write loop for the party in song
+            
+ 
+soup_to_text()
 
 its_time_to_party = input("Are you a party animal? 'Y' or 'N'")
 we_will_party_hard = None
