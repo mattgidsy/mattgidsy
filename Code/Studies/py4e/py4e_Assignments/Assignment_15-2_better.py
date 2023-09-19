9c30f01c419b067bf4d249d91f1572817d42200b
mattgidsy go <62821667+mattgidsy@users.noreply.github.com>
2023-09-18 19:28:34 -0500

    finally fixed it

--- Code/Studies/py4e/py4e_Assignments/Assignment_15-2_better.py
+++ Code/Studies/py4e/py4e_Assignments/Assignment_15-2_better.py
@@ -1,7 +1,7 @@
 import sqlite3
 
 
-con = sqlite3.connect('orgdb_fast')
+con = sqlite3.connect('orgdb_fast.sqlite')
 curse = con.cursor()
 curse.execute('DROP TABLE IF EXISTS Orgs')
 curse.execute('CREATE TABLE Orgs (org TEXT, count INTERGER)')
@@ -10,6 +10,7 @@ curse.execute('CREATE TABLE Orgs (org TEXT, count INTERGER)')
 def search_split():
     file_name = 'mbox.txt'
     commit_count = 0
+    lines_count = 0
     try:
         handle = open(file_name)
     except Exception as e:
@@ -20,15 +21,19 @@ def search_split():
         org = slices[1]
         curse.execute('SELECT count FROM Orgs WHERE org =? ', (org,))
         row = curse.fetchone()
-        commit_count = commit_count +1
-        if commit_count == 1:
-            con.commit()
-            #commit_count = 0
-        elif row is None:
+        lines_count = lines_count +1
+        if row is None:
             curse.execute('INSERT INTO Orgs (org, count) VALUES (?,1)', (org,))
-        else:
-            curse.execute('UPDATE Orgs SET count = count +1 WHERE org = ?', (org,))       
+        if row is not None:
+            curse.execute('UPDATE Orgs SET count = count +1 WHERE org = ?', (org,)) 
+        if lines_count % 100 == 0:
+            lines_count = 0
+            commit_count += 1
+            con.commit()
+    con.commit()
+           
     print('Commit count:',commit_count)
+    
 
 search_split()
 sqlstr = 'SELECT org, count FROM Orgs ORDER BY count DESC LIMIT 10'
