d6eac19cece6bb7490a886b037591d59b68e78c3
mattgidsy <62821667+mattgidsy@users.noreply.github.com>
2023-09-20 19:08:02 -0500

    testing weird

--- Code/Studies/py4e/py4e_ Notes/Testing Scratch.py
+++ Code/Studies/py4e/py4e_ Notes/Testing Scratch.py
@@ -1,17 +1,2 @@
-#create class
-class Bike:
-    #name and gear are attributes of bike
-    name = ""
-    gear = 0
-
-#create objects of class
-bike1 = Bike()
- #access value of the attribute, gear, in the bike object
-print(bike1.gear)
-
-#modify or assign new values 
-bike1.gear = 420
-bike1.name = "Montain Bike"
-
-print(bike1.gear)
-print(f"Name: {bike1.name}  Gears: {bike1.gear}")
+fh= open('words.txt')
+print(fh)
