06e49741ce7705c83971c45e21fdb9c44b547acb
mattgidsy <62821667+mattgidsy@users.noreply.github.com>
2023-10-11 20:55:33 -0500

    daily

---
+++ Code/Studies/p3p/p3pTesting.py
@@ -0,0 +1,15 @@
+
+by = "You are"
+az = "doing a great "
+io = "job"
+qy = "keep it up!"
+
+msglist = list()
+thing = by + az + io + qy
+for word in thing:
+    word.split()
+    msglist.append(word)
+    
+message = " ".join(msglist)
+
+print(msglist)
