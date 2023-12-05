482f9f934bd8bf14691a2d0bbc03bfdeaa01345b
mattgidsy go <62821667+mattgidsy@users.noreply.github.com>
2023-12-04 20:47:50 -0600

    laptop update

---
+++ Code/Studies/p3p/Assessments/c4_04_02.py
@@ -0,0 +1,10 @@
+
+di = [{"Puppies": 17, 'Kittens': 9, "Birds": 23, 'Fish': 90, "Hamsters": 49}, {"Puppies": 23, "Birds": 29, "Fish": 20, "Mice": 20, "Snakes": 7}, {"Fish": 203, "Hamsters": 93, "Snakes": 25, "Kittens": 89}, {"Birds": 20, "Puppies": 90, "Snakes": 21, "Fish": 10, "Kittens": 67}]
+total = 0
+for diction in di:
+    try:
+        print(diction["Puppies"])
+    except:
+        continue
+
+print("Total number of puppies:", total)
--- Code/Studies/p3p/p3pTesting.py
+++ Code/Studies/p3p/p3pTesting.py
@@ -1,13 +1,10 @@
-class Cereal:
-    def __init__(self, name, brand, fiber):
-        self.n = name
-        self.b = brand
-        self.f = fiber
-    def __str__(self):
-        return f"{self.n} cereal is produced by {self.b} and has {self.f} grams of fiber in every serving!"
+def distance(x1, y1, x2, y2):
+    dx = x2 - x1
+    dy = y2 - y1
+    dsquared = dx**2 + dy**2
+    result = dsquared**0.5
+    return result
 
-c1 = Cereal("Cornflakes", "Kellog's", '2')
-c2 = Cereal('Honey Nut Cheerios', 'General Mills', '3')
-
-print(c1)
-print(c2)
+assert distance(1, 2, 1, 2) == 0
+assert distance(1,2, 4,6) == 5
+assert distance(0,0, 1,1) == 2**0.5
