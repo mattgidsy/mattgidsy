53d4c3f4bc88af1a635391187832ec541b07ec19
mattgidsy <62821667+mattgidsy@users.noreply.github.com>
2023-12-06 12:51:37 -0600

    finished WOP Project!

--- Code/Studies/p3p/Assessments/testing1234.py
+++ Code/Studies/p3p/Assessments/testing1234.py
@@ -10,3 +10,12 @@ if 2 < 250:
             print('pass')
     print(guess_lst)
 
+guess_lst = []
+        no_guess = []
+        if self.prizeMoney < VOWEL_COST:
+            for c in LETTERS:
+                if c in guessed:
+                    no_guess.append(c)
+                elif c in VOWELS:
+                    no_guess.append(c) 
+            return no_guess
