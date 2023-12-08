4efda6404b691fe373a4f87e1c068ceed9d464bf
mattgidsy go <62821667+mattgidsy@users.noreply.github.com>
2023-12-07 21:29:15 -0600

    night commit

--- Code/Studies/p3p/Assessments/testing1234.py
+++ Code/Studies/p3p/Assessments/testing1234.py
@@ -1,21 +1,32 @@
 LETTERS = 'ABCDEFGH'
 VOWELS = 'AEIOU'
 
-guessed = ['B','C','D','F','G','H']
+guess1 = 'tAbLe'
+guess_cl = 'le'
+words = ["apple", "table", "chair", "house", "mouse", "plant", "bread", "water", "earth", "music"]
 
-guess_lst = []
-if 2 < 250:
-    for c in LETTERS:
-        if c in guessed and c not in VOWELS:
-            print('pass')
-    print(guess_lst)
+# Creating a list of tuples
+word_tuples = [(word, list(word)) for word in words]
+possible_tups = []
+# Printing the result
+# for (word, letters) in word_tuples:
+#     if "n" == letters[3]:
+#         if (word,letters) not in possible_tups:
+#             possible_tups.append((word,letters))
 
-guess_lst = []
-        no_guess = []
-        if self.prizeMoney < VOWEL_COST:
-            for c in LETTERS:
-                if c in guessed:
-                    no_guess.append(c)
-                elif c in VOWELS:
-                    no_guess.append(c) 
-            return no_guess
+#print(possible_tups)
+# def find_upper():
+#     index = -1
+#     for char in guess1:
+#         index += 1
+#         if char.isupper():
+#             print(index)
+
+# find_upper()
+
+def check_correct_place(guess, guess_cl):
+    for index, char in enumerate(guess):
+        if char.isupper():
+            print(index) 
+            
+check_correct_place(guess1,guess_cl)
--- Code/Wordle_bot/wordle_hint_bot.py
+++ Code/Wordle_bot/wordle_hint_bot.py
@@ -1,20 +1,41 @@
 ##### wordle helper by inquiz #####
 
 #open the file containing a list of 5 letter words
-with open("sample_list.txt") as guess_list:
+with open("Code\Wordle_bot\guess_list.txt") as guess_list:
     lines = guess_list.readlines()
     
 word_list = [word.strip() for word in lines]
 word_tuples = [(word, list(word)) for word in word_list]
+possible_guess_lst = []
 
-possible_guess_lst = word_list[:10]
+#Need to write 4 searches
+#correct letter(s)
+#correct letter(s) placement
+#excluded letter(s)
+#incorrect placement
 
-class GetPossibleGuesses:
-    
-    def __init__(self,  ):
-        
-    
-    
+# def find_upper():
+#     index = -1
+#     #find a way to deal with more than 1 uppercase
+#     for char in guess1:
+#         index += 1
+#         if char.isupper():
+#             return index
+ 
+                  
+def check_correct_place(guess, guess_cl):
+    for index, char in enumerate(guess):
+        if char.isupper():
+            print(index) 
+
+def check_correct_letter(guess, guess_cl):
+    guess_letters = list(guess_cl)
+    for word in word_list:
+        # Check if at least one of the specified letters is present in the word
+        if all(letter in word for letter in guess_letters):
+            if word not in possible_guess_lst:
+            # If true, append the word to the filtered list if it's not there
+                possible_guess_lst.append(word)
 
 def possible_guess_results():
     if possible_guess_lst == []:
@@ -31,7 +52,7 @@ def get_started():
     print("\n   ###### Welcome to Wordle_Helper_Bot! ######")
     guess1 = input("\nInput your first guess:\n")
     guess1_cl = input("\nWhich letters are in the word but out of position?:\n")
-    GetPossibleGuesses
+    check_correct_letter(guess1,guess1_cl)
     possible_guess_results()    
 
 
