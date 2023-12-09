0ebe9c37b74c1000b82f55d99b660a5a867fc682
mattgidsy <62821667+mattgidsy@users.noreply.github.com>
2023-12-08 18:24:16 -0600

    correct letter function

--- Code/Studies/p3p/Assessments/testing1234.py
+++ Code/Studies/p3p/Assessments/testing1234.py
@@ -1,13 +1,15 @@
-LETTERS = 'ABCDEFGH'
-VOWELS = 'AEIOU'
+#https://www.aeio.win/answers.txt
+#https://www.aeio.win/guesses.txt
+# LETTERS = 'ABCDEFGH'
+# VOWELS = 'AEIOU'
 
-guess1 = 'tAbLe'
-guess_cl = 'le'
-words = ["apple", "table", "chair", "house", "mouse", "plant", "bread", "water", "earth", "music"]
+# guess1 = 'tAbLe'
+# guess_cl = 'le'
+# words = ["apple", "table", "chair", "house", "mouse", "plant", "bread", "water", "earth", "music"]
 
-# Creating a list of tuples
-word_tuples = [(word, list(word)) for word in words]
-possible_tups = []
+# # Creating a list of tuples
+# word_tuples = [(word, list(word)) for word in words]
+# possible_tups = []
 # Printing the result
 # for (word, letters) in word_tuples:
 #     if "n" == letters[3]:
@@ -24,9 +26,61 @@ possible_tups = []
 
 # find_upper()
 
-def check_correct_place(guess, guess_cl):
-    for index, char in enumerate(guess):
-        if char.isupper():
-            print(index) 
+# def check_correct_place(guess, guess_cl):
+#     for index, char in enumerate(guess):
+#         if char.isupper():
+#             print(index) 
             
-check_correct_place(guess1,guess_cl)
+# check_correct_place(guess1,guess_cl)
+
+def check_correct_place(guess):
+    possible_tups = []
+    
+    # Create a list of (lowercase letter, index) tuples for the guessed letters
+    guessed_positions = [(char.lower(), index) for index, char in enumerate(guess) if char.isupper()]
+
+    for word, letters in word_tuples:
+        # Check if all guessed letters are in the correct positions
+        if all((g, i) in guessed_positions and g == letters[i] for g, i in guessed_positions):
+            possible_tups.append((word, letters))
+                    
+    return possible_tups
+
+# Example usage:
+words = ["slate", "slide", "sound", "salad"]
+word_tuples = [(word, list(word)) for word in words]
+
+guess = "SlatD"
+result = check_correct_place(guess)
+print(result)
+# print(possible_tups)
+
+# def find_upper():
+#     index = -1
+#     #find a way to deal with more than 1 uppercase
+#     for char in guess1:
+#         index += 1
+#         if char.isupper():
+#             return index
+
+#original
+# def check_correct_place(guess, guess_cl):
+#     for index, char in enumerate(guess):
+#         if char.isupper():
+#             for word,letters in word_tuples:
+#                  if char.lower() == letters[index]:
+#                      possible_tups.append((word,letters))
+                     
+#chat modified
+# def check_correct_place(guess):
+#     possible_tups = []
+    
+#     # Create a list of (lowercase letter, index) tuples for the guessed letters
+#     guessed_positions = [(char.lower(), index) for index, char in enumerate(guess) if char.isupper()]
+
+#     for word, letters in word_tuples:
+#         # Check if all guessed letters are in the correct positions
+#         if all((g, i) in guessed_positions and g == letters[i] for g, i in guessed_positions):
+#             possible_tups.append((word, letters))
+                    
+#     return possible_tups
--- Code/Wordle_bot/wordle_hint_bot.py
+++ Code/Wordle_bot/wordle_hint_bot.py
@@ -7,27 +7,26 @@ with open("Code\Wordle_bot\guess_list.txt") as guess_list:
 word_list = [word.strip() for word in lines]
 word_tuples = [(word, list(word)) for word in word_list]
 possible_guess_lst = []
+possible_tups = []
 
 #Need to write 4 searches
 #correct letter(s)
 #correct letter(s) placement
 #excluded letter(s)
 #incorrect placement
+               
+def check_correct_place(guess,guess_cl):
+    
+    # Create a list of (lowercase letter, index) tuples for the guessed letters
+    guessed_positions = [(char.lower(), index) for index, char in enumerate(guess) if char.isupper()]
 
-# def find_upper():
-#     index = -1
-#     #find a way to deal with more than 1 uppercase
-#     for char in guess1:
-#         index += 1
-#         if char.isupper():
-#             return index
- 
-                  
-def check_correct_place(guess, guess_cl):
-    for index, char in enumerate(guess):
-        if char.isupper():
-            print(index) 
+    for word, letters in word_tuples:
+        # Check if all guessed letters are in the correct positions
+        if all((g, i) in guessed_positions and g == letters[i] for g, i in guessed_positions) and (word, letters) not in possible_tups:
+            possible_tups.append((word, letters))
 
+                                  
+#this needs to be re-worked to use the tuples list
 def check_correct_letter(guess, guess_cl):
     guess_letters = list(guess_cl)
     for word in word_list:
@@ -52,9 +51,10 @@ def get_started():
     print("\n   ###### Welcome to Wordle_Helper_Bot! ######")
     guess1 = input("\nInput your first guess:\n")
     guess1_cl = input("\nWhich letters are in the word but out of position?:\n")
-    check_correct_letter(guess1,guess1_cl)
-    possible_guess_results()    
-
+    check_correct_place(guess1,guess1_cl)
+    #check_correct_letter(guess1,guess1_cl)
+    print(possible_tups)
+    #possible_guess_results()    
 
 
 get_started()
