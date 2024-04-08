939ca204c38b058337b93a00e769fee80a166a3c
mattgidsy <62821667+mattgidsy@users.noreply.github.com>
2024-04-08 15:41:35 -0500

    random update

--- Code/Studies/FCC_backend_dev/FullStack_intro/movieProjectBackend/api/reviews.route.js
+++ Code/Studies/FCC_backend_dev/FullStack_intro/movieProjectBackend/api/reviews.route.js
@@ -1,7 +1,14 @@
-import express from "express";
+import express from "express"
+import ReviewsCtrl from "./reviews.controller.js"
 
-const router = express.Router();
+const router = express.Router()
 
-router.route("/").get((req, res) => res.send("hello world"));
+router.route("/movie/:id").get(ReviewsCtrl.apiGetReviews)
+router.route("/new").post(ReviewsCtrl.apiPostReview)
+router.route("/:id")
+    .get(ReviewsCtrl.apiGetReview)
+    .put(ReviewsCtrl.apiUpdateReview)
+    .delete(ReviewsCtrl.apiDeleteReview)
+
 
 export default router;
