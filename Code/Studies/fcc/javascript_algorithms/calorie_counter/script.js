040f209a623054f2d357d20e56c6e5536c5b63d3
mattgidsy go <62821667+mattgidsy@users.noreply.github.com>
2024-12-22 09:54:08 -0600

    laptop save

--- Code/Studies/fcc/javascript_algorithms/calorie_counter/script.js
+++ Code/Studies/fcc/javascript_algorithms/calorie_counter/script.js
@@ -16,3 +16,11 @@ function isInvalidInput(str) {
     const regex = /\d+e\d+/i;
     return str.match(regex);
 }
+
+function addEntry() {
+    const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
+    const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length;
+    const HTMLString = `<label for="${entryDropdown.value}-${entryNumber}-name" >Entry ${entryNumber} Name </label>`; 
+}
+
+ 
