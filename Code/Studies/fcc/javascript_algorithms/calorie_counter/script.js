ebb8a61997f06b9ff8bbfc382114d9d9b6e6570f
mattgidsy <62821667+mattgidsy@users.noreply.github.com>
2025-01-12 15:37:55 -0600

    backup

--- Code/Studies/fcc/javascript_algorithms/calorie_counter/script.js
+++ Code/Studies/fcc/javascript_algorithms/calorie_counter/script.js
@@ -1,26 +1,27 @@
-const calorieCounter = document.getElementById('calorie-counter');
-const budgetNumberInput = document.getElementById('budget');
-const entryDropdown = document.getElementById('entry-dropdown');
-const addEntryButton = document.getElementById('add-entry');
-const clearButton = document.getElementById('clear');
-const output = document.getElementById('output')
+const calorieCounter = document.getElementById("calorie-counter");
+const budgetNumberInput = document.getElementById("budget");
+const entryDropdown = document.getElementById("entry-dropdown");
+const addEntryButton = document.getElementById("add-entry");
+const clearButton = document.getElementById("clear");
+const output = document.getElementById("output");
 let isError = false;
 
 function cleanInputString(str) {
-    const regex = /[+-\s]/g;
-    return str.replace(regex, "");
-    
-} 
+  const regex = /[+-\s]/g;
+  return str.replace(regex, "");
+}
 
 function isInvalidInput(str) {
-    const regex = /\d+e\d+/i;
-    return str.match(regex);
+  const regex = /\d+e\d+/i;
+  return str.match(regex);
 }
 
 function addEntry() {
-    const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
-    const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length;
-    const HTMLString = `<label for="${entryDropdown.value}-${entryNumber}-name" >Entry ${entryNumber} Name </label>`; 
+  const targetInputContainer = document.querySelector(
+    `#${entryDropdown.value} .input-container`
+  );
+  const entryNumber =
+    targetInputContainer.querySelectorAll('input[type="text"]').length;
+  const HTMLString = `<label for="${entryDropdown.value}-${entryNumber}-name" >Entry ${entryNumber} Name </label>
+                        <input type="text" placeholder="Name" id="${entryDropdown.value}-${entryNumber}-name">`;
 }
-
- 
